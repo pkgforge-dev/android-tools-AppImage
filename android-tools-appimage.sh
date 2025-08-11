7cbe483b3e98c85d4eb42e72d00ef4bc147dafa7
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-10 23:39:52 -0400

    Update android-tools-appimage.sh

--- android-tools-appimage.sh
+++ android-tools-appimage.sh
@@ -6,7 +6,6 @@ ARCH="$(uname -m)"
 URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 BINARY="https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
-ICON="https://github.com/pkgforge-dev/android-tools-AppImage/blob/main/Android.png?raw=true"
 UDEV="https://raw.githubusercontent.com/M0Rf30/android-udev-rules/refs/heads/main/51-android.rules"
 
 export ADD_HOOKS="udev-installer.hook"
@@ -15,10 +14,9 @@ export URUNTIME_PRELOAD=1 # really needed here
 
 # CREATE DIRECTORIES AND DOWNLOAD THE ARCHIVE
 mkdir -p ./AppDir/shared/bin ./AppDir/bin ./AppDir/etc/udev/rules.d
-wget "$BINARY" -O ./bin.zip
+wget --retry-connrefused --tries=30 "$BINARY" -O ./bin.zip
 unzip -q ./bin.zip
 rm -f ./bin.zip
-cp -vr ./platform-tools/lib64       ./AppDir/bin
 cp -v  ./platform-tools/mke2fs.conf ./AppDir/bin
 mv -v  ./platform-tools/*           ./AppDir/shared/bin
 
